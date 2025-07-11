5a2fce88bdefe3588bf0fd8016640c5b38797924
Erik Segura <158991388+Zatrayu84@users.noreply.github.com>
2025-07-11 16:38:36 -0500

    Created seperated files for easier coding, now separating from window.cpp

---
+++ Windows_Test/App.h
@@ -0,0 +1,40 @@
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include "Player.h"
+#include "Enemy.h"
+#include "Bullet.h"
+#include "Audio.h"
+
+class myApp
+{
+public:
+    myApp();
+    void runMe();
+
+private:  
+    //  variables for the game go here
+    sf::RenderWindow newWindow;
+    sf::Clock myClock;
+    bool goTextActive = false;
+    float bulletSpeed = 200.f;
+
+    //  Game Objects here
+    Player::myPlayer;
+    Audio myAudio;
+    std::vector<Enemy> enemies;
+    std::vector<Bullets> bullets;
+
+    //  SFML assets
+    sf::Sprite background;
+    sf::Text textGameOver;
+    sf::Font fontGameOver;
+    sf::Texture texture;
+
+    //  Helper methods for loading and intiailizing enemies
+    void loadAssets();
+    void initializeEnemies();
+    void handleEvents();
+    void updateLogic(float deltaTime);
+    void render();
+
+};
